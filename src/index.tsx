68135076cb54858ebf66321ab0a5e5879c6d1664
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-08-03 17:37:26 -0400

    Setting global DEV variable.
    Used to avoid registering service worker in dev.

--- src/index.tsx
+++ src/index.tsx
@@ -12,22 +12,27 @@ import ErrorBoundary from './ErrorBoundary';
 const App = React.lazy(()=>import('./App'));
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <React.Suspense fallback={<Loading />}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </React.Suspense>
-  </React.StrictMode>
+	<React.StrictMode>
+		<React.Suspense fallback={<Loading />}>
+			<ErrorBoundary>
+				<App />
+			</ErrorBoundary>
+		</React.Suspense>
+	</React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+
+// A global var is set in index.html to allow detecting if we're in a dev environment.
+// @ts-ignore
+if(GLOBAL_DEV_FLAG) {
+   	serviceWorkerRegistration.unregister();
+} else {
+	// Assume production environment
+	serviceWorkerRegistration.register();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
