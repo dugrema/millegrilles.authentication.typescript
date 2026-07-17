909a763663c88367df7e093a5ad9bc24180d3610
Mathieu Dugre <mathieu.dugre@mdugre.info>
2026-07-17 13:14:51 -0400

    Fix tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
-const flowbite = require("flowbite-react/tailwind");
+const flowbitePlugin = require("flowbite-react/plugin/tailwindcss");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{ts,tsx}/",
-    flowbite.content()
+    "./src/**/*.{ts,tsx}",
+    "./node_modules/flowbite-react/dist/esm/**/*.js",
+    "./node_modules/flowbite-react/dist/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
-    flowbite.plugin(),
+    flowbitePlugin,
   ],
 }
 
