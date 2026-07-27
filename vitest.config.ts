ea26a852b6fb8409559bb240a798bb10c2418ef8
Mathieu Dugre <mathieu.dugre@mdugre.info>
2026-07-27 12:43:42 -0400

    Fixing MQ_HOSTNAME catalog config
    Adding resolver for apiMapping.json

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,60 +2,76 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import path from 'path';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          NODE_ENV: process.env.NODE_ENV || 'development',
-        },
-      },
-    }),
-    VitePWA({
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'service-worker.ts',
-      registerType: 'auto',
-      injectManifest: {
-        swDest: 'dist/service-worker.js',
-      },
-      manifest: {
-        name: 'MilleGrilles',
-        short_name: 'MilleGrilles',
-        description: 'MilleGrilles Authentication',
-        start_url: '/millegrilles/',
-        display: 'standalone',
-        background_color: '#021024',
-        theme_color: '#021024',
-        icons: [
-          {
-            src: '/favicon.ico',
-            sizes: '64x64',
-            type: 'image/x-icon',
-          },
-          {
-            src: '/millegrilles_512.jpg',
-            sizes: '512x512',
-            type: 'image/jpeg',
-          },
-          {
-            src: '/millegrilles_192.jpg',
-            sizes: '192x192',
-            type: 'image/jpeg',
-          },
-        ],
-      },
-    }),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    server: {
-      deps: {
-        inline: ['@dugrema/node-forge'],
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+
+  return ({
+    resolve: {
+      alias: {
+        // Intercept imports to use the correct files during build
+        '@apiMapping-json': isBuild
+          ? path.resolve(__dirname, 'src/workers/apiMapping.signed.json')
+          : path.resolve(__dirname, 'src/workers/apiMapping.json'),
+        '@manifest-build-json': isBuild 
+          ? path.resolve(__dirname, 'build_assets/manifest.build.json')
+          : path.resolve(__dirname, 'src/manifest.build.json'),
       },
     },
-  },
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            NODE_ENV: process.env.NODE_ENV || 'development',
+          },
+        },
+      }),
+      VitePWA({
+        strategies: 'injectManifest',
+        srcDir: 'src',
+        filename: 'service-worker.ts',
+        registerType: 'auto',
+        injectManifest: {
+          swDest: 'dist/service-worker.js',
+        },
+        manifest: {
+          name: 'MilleGrilles',
+          short_name: 'MilleGrilles',
+          description: 'MilleGrilles Authentication',
+          start_url: '/millegrilles/',
+          display: 'standalone',
+          background_color: '#021024',
+          theme_color: '#021024',
+          icons: [
+            {
+              src: '/favicon.ico',
+              sizes: '64x64',
+              type: 'image/x-icon',
+            },
+            {
+              src: '/millegrilles_512.jpg',
+              sizes: '512x512',
+              type: 'image/jpeg',
+            },
+            {
+              src: '/millegrilles_192.jpg',
+              sizes: '192x192',
+              type: 'image/jpeg',
+            },
+          ],
+        },
+      }),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      server: {
+        deps: {
+          inline: ['@dugrema/node-forge'],
+        },
+      },
+    }
+  })
 });
